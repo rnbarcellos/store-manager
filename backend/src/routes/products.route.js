250dfb6772fb9fc45c621cd7e72849e8a61e221e
rnbarcellos <rodrigo.barcellos@outlook.com.br>
2023-09-14 14:49:25 -0300

    :sparkles: feat: create product routes
    routes createad: /products and /products/:id

--- backend/src/routes/products.route.js
+++ backend/src/routes/products.route.js
@@ -1,5 +1,9 @@
 const { Router } = require('express');
+const { productsController } = require('../controllers');
 
 const productsRouter = Router();
+
+productsRouter.get('/', productsController.showAllProducts);
+productsRouter.get('/:id', productsController.showProductById);
 
 module.exports = productsRouter;
