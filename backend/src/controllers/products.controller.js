c9ec7ed59fc5ed16031f551d3575df117b164bb8
rnbarcellos <rodrigo.barcellos@outlook.com.br>
2023-09-14 14:47:55 -0300

    :wrench: chore: create controller layer

---
+++ backend/src/controllers/products.controller.js
@@ -0,0 +1,12 @@
+const showAllProducts = async (req, res) => {
+  console.log(req, res);
+};
+
+const showProductById = async (req, res) => {
+  console.log(req, res);
+};
+
+module.exports = {
+  showAllProducts,
+  showProductById,
+};
