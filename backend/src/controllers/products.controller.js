50f0233a25e166ac85af3ba60b003eb356dca322
rnbarcellos <rodrigo.barcellos@outlook.com.br>
2023-09-14 16:09:22 -0300

    :sparkles: feat: write code for showAllProducts and showProductById functions on controller

--- backend/src/controllers/products.controller.js
+++ backend/src/controllers/products.controller.js
@@ -1,9 +1,14 @@
+const { productsService } = require('../services');
+
 const showAllProducts = async (req, res) => {
-  console.log(req, res);
+  const products = await productsService.showAllProducts();
+  return res.status(products.status).json(products.data);
 };
 
 const showProductById = async (req, res) => {
-  console.log(req, res);
+  const { id } = req.params;
+  const product = await productsService.showProductById(id);
+  return res.status(product.status).json(product.data);
 };
 
 module.exports = {
