f68950d03af441b508978e3aee9c482f9bada82b
rnbarcellos <rodrigo.barcellos@outlook.com.br>
2023-09-14 14:36:41 -0300

    :wrench: chore: create products and sales routes files

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const { productsRouter, salesRouter } = require('./routes');
 
 const app = express();
+
+app.use(express.json());
+app.use('/products', productsRouter);
+app.use('/sales', salesRouter);
 
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
---
+++ backend/src/routes/index.js
@@ -0,0 +1,7 @@
+const productsRouter = require('./products.route');
+const salesRouter = require('./sales.route');
+
+module.exports = {
+  productsRouter,
+  salesRouter,
+};
