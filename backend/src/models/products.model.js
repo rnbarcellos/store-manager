8d6ba97c2248d58b962111b5586d7e463183fa0c
rnbarcellos <rodrigo.barcellos@outlook.com.br>
2023-09-14 15:13:18 -0300

    :sparkles: feat: create findAll and findById functions on model layer

---
+++ backend/src/models/products.model.js
@@ -0,0 +1,21 @@
+const connection = require('./connection');
+
+const findAll = async () => {
+  const [products] = await connection.execute(
+    'SELECT * FROM products',
+  );
+  return products;
+};
+
+const findById = async (id) => {
+  const [product] = await connection.execute(
+    'SELECT * FROM products WHERE id = ?',
+    [id],
+  );
+  return product;
+};
+
+module.exports = {
+  findAll,
+  findById,
+};
