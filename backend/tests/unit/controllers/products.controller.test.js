ffc7bde5d0cc31dd08c382950d7e6d671a047a44
rnbarcellos <rodrigo.barcellos@outlook.com.br>
2023-09-15 12:19:42 -0300

    :test_tube: test: develops tests for controller layer

--- backend/tests/unit/controllers/products.controller.test.js
+++ backend/tests/unit/controllers/products.controller.test.js
@@ -1,6 +1,49 @@
-// TODO: desenvolver testes unitários para o controller de produtos
+const sinon = require('sinon');
+const chai = require('chai');
+const { productsService } = require('../../../src/services');
+const { productsController } = require('../../../src/controllers');
+const { allProductsFromDB, productByIdFromDB } = require('../../mocks/products.mock');
+
+const { expect } = chai;
+
 describe('Testando o controller de produtos', function () {
-  it('Será validado que é possível listar todos os produtos', function () {
-    // ...
+  it('Será validado que é possível listar todos os produtos', async function () {
+    sinon.stub(productsService, 'showAllProducts').resolves({
+      status: 200,
+      data: allProductsFromDB,
+    });
+
+    const res = {
+      status: sinon.stub().returnsThis(),
+      json: sinon.stub(),
+    };
+
+    await productsController.showAllProducts({}, res);
+
+    expect(res.status.calledWith(200)).to.be.equal(true);
+    expect(res.json.calledWith(allProductsFromDB)).to.be.equal(true);
+  });
+
+  it('Será validado que é possível listar um produto pelo id', async function () {
+    sinon.stub(productsService, 'showProductById').resolves({
+      status: 200,
+      data: productByIdFromDB,
+    });
+
+    const req = {
+      params: {
+        id: 1,
+      },
+    };
+
+    const res = {
+      status: sinon.stub().returnsThis(),
+      json: sinon.stub(),
+    };
+
+    await productsController.showProductById(req, res);
+
+    expect(res.status.calledWith(200)).to.be.equal(true);
+    expect(res.json.calledWith(productByIdFromDB)).to.be.equal(true);
   });
 });
