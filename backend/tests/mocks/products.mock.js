d40a83a22b7b7afaedb8cb709dd1cd6da77cf660
rnbarcellos <rodrigo.barcellos@outlook.com.br>
2023-09-15 12:20:17 -0300

    :wrench: chore: add integration tests folder

---
+++ backend/tests/mocks/products.mock.js
@@ -0,0 +1,24 @@
+const allProductsFromDB = [
+  {
+    id: 1,
+    name: 'Martelo de Thor',
+  },
+  {
+    id: 2,
+    name: 'Traje de encolhimento',
+  },
+  {
+    id: 3,
+    name: 'Escudo do Capitão América',
+  },
+];
+
+const productByIdFromDB = {
+  id: 1,
+  name: 'Martelo de Thor',
+};
+
+module.exports = {
+  allProductsFromDB,
+  productByIdFromDB,
+};
