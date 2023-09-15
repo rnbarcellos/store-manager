583e2dd3d1b875bfa3e0924c5cc831cbdb534a67
rnbarcellos <rodrigo.barcellos@outlook.com.br>
2023-09-15 12:37:45 -0300

    :feat: create /sales and /sales/:id endpoints

--- backend/src/routes/sales.route.js
+++ backend/src/routes/sales.route.js
@@ -1,5 +1,9 @@
 const { Router } = require('express');
+const { salesController } = require('../controllers');
 
 const salesRouter = Router();
+
+salesRouter.get('/', salesController.showAllSales);
+salesRouter.get('/:id', salesController.showSaleById);
 
 module.exports = salesRouter;
