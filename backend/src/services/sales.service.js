05d68bfbb729f90aa2350681800a1773d6ce27cc
rnbarcellos <rodrigo.barcellos@outlook.com.br>
2023-09-15 12:34:37 -0300

    :feat: create showAllSales and showSaleById on service layer

---
+++ backend/src/services/sales.service.js
@@ -0,0 +1,31 @@
+const { salesModel } = require('../models');
+
+const showAllSales = async () => {
+  const sales = await salesModel.findAll();
+  return {
+    status: 200,
+    data: sales,
+  };
+};
+
+const showSaleById = async (id) => {
+  const sale = await salesModel.findById(id);
+  if (!sale) {
+    return {
+      status: 404,
+      data: {
+        message: 'Sale not found',
+      },
+    };
+  }
+
+  return {
+    status: 200,
+    data: sale,
+  };
+};
+
+module.exports = {
+  showAllSales,
+  showSaleById,
+};
