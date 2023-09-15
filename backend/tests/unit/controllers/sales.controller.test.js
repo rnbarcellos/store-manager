7760ff9d87db9c12c2bb1191591f848bc29f039d
rnbarcellos <rodrigo.barcellos@outlook.com.br>
2023-09-15 13:54:17 -0300

    :test_tube: test: create tests for showAllSales and showSaleById of controller

--- backend/tests/unit/controllers/sales.controller.test.js
+++ backend/tests/unit/controllers/sales.controller.test.js
@@ -1,5 +1,48 @@
+const { expect } = require('chai');
+const sinon = require('sinon');
+const { salesController } = require('../../../src/controllers');
+const { salesService } = require('../../../src/services');
+const { allSalesFromDb, salesFromDb } = require('../../mocks/sales.mocks');
+
 describe('Testa o controller de sales', function () {
-  it('Testa se o controller de sales retorna um objeto', async function () {
-    // todo
+  it('Será validado que é possível listar todas as vendas', async function () {
+    sinon.stub(salesService, 'showAllSales').resolves({
+      status: 200,
+      data: allSalesFromDb,
+    });
+
+    const res = {
+      status: sinon.stub().returnsThis(),
+      json: sinon.stub(),
+    };
+    const req = {};
+
+    await salesController.showAllSales(req, res);
+
+    expect(res.status.calledWith(200)).to.be.equal(true);
+    expect(res.json.calledWith(allSalesFromDb)).to.be.equal(true);
+  });
+
+  it('Será validado que é possível listar uma venda pelo id', async function () {
+    sinon.stub(salesService, 'showSaleById').resolves({
+      status: 200,
+      data: salesFromDb,
+    });
+
+    const req = {
+      params: {
+        id: 1,
+      },
+    };
+
+    const res = {
+      status: sinon.stub().returnsThis(),
+      json: sinon.stub(),
+    };
+
+    await salesController.showSaleById(req, res);
+
+    expect(res.status.calledWith(200)).to.be.equal(true);
+    expect(res.json.calledWith(salesFromDb)).to.be.equal(true);
   });
 });
