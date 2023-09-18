ecccc302496ae618f71a4b6e3ed6fef1ed7965bd
rnbarcellos <rodrigo.barcellos@outlook.com.br>
2023-09-18 14:58:18 -0300

    :test_tube: test: two new cases for controller
    a test for showSaleById and another one for createNewSale()

--- backend/tests/unit/controllers/sales.controller.test.js
+++ backend/tests/unit/controllers/sales.controller.test.js
@@ -2,9 +2,13 @@ const { expect } = require('chai');
 const sinon = require('sinon');
 const { salesController } = require('../../../src/controllers');
 const { salesService } = require('../../../src/services');
-const { allSalesFromDb, salesFromDb } = require('../../mocks/sales.mocks');
+const { allSalesFromDb, salesFromDb, salesCreated, itemsSold } = require('../../mocks/sales.mocks');
 
 describe('Testa o controller de sales', function () {
+  afterEach(function () {
+    sinon.restore();
+  });
+
   it('Será validado que é possível listar todas as vendas', async function () {
     sinon.stub(salesService, 'showAllSales').resolves({
       status: 200,
@@ -44,5 +48,53 @@ describe('Testa o controller de sales', function () {
 
     expect(res.status.calledWith(200)).to.be.equal(true);
     expect(res.json.calledWith(salesFromDb)).to.be.equal(true);
+  });
+
+  it('Será validado que é possível listar uma venda pelo id e retorna erro quando a venda não existe', async function () {
+    sinon.stub(salesService, 'showSaleById').resolves({
+      status: 404,
+      data: {
+        message: 'Sale not found',
+      },
+    });
+
+    const req = {
+      params: {
+        id: 4,
+      },
+    };
+
+    const res = {
+      status: sinon.stub().returnsThis(),
+      json: sinon.stub(),
+    };
+
+    await salesController.showSaleById(req, res);
+
+    expect(res.status.calledWith(404)).to.be.equal(true);
+    expect(res.json.calledWith({
+      message: 'Sale not found',
+    })).to.be.equal(true);
+  });
+
+  it('Será validado que é possível criar uma venda', async function () {
+    sinon.stub(salesService, 'createNewSale').resolves({
+      status: 201,
+      data: salesCreated,
+    });
+
+    const req = {
+      body: itemsSold,
+    };
+
+    const res = {
+      status: sinon.stub().returnsThis(),
+      json: sinon.stub(),
+    };
+
+    await salesController.createNewSale(req, res);
+
+    expect(res.status.calledWith(201)).to.be.equal(true);
+    expect(res.json.calledWith(salesCreated)).to.be.equal(true);
   });
 });
