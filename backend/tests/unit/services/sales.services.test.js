e0df42aa4ea0b5e92f1966c08f9880a0560f498c
rnbarcellos <rodrigo.barcellos@outlook.com.br>
2023-09-18 14:48:52 -0300

    :test_tube: test: createNewSale() of service

--- backend/tests/unit/services/sales.services.test.js
+++ backend/tests/unit/services/sales.services.test.js
@@ -2,7 +2,7 @@ const { expect } = require('chai');
 const sinon = require('sinon');
 const { salesService } = require('../../../src/services');
 const { salesModel } = require('../../../src/models');
-const { allSalesFromDb, salesFromDb } = require('../../mocks/sales.mocks');
+const { allSalesFromDb, salesFromDb, itemsSold } = require('../../mocks/sales.mocks');
 
 describe('Testa o service de sales', function () {
   afterEach(function () {
@@ -49,5 +49,30 @@ describe('Testa o service de sales', function () {
     expect(response.data).to.be.an('object');
     expect(response.data).to.have.property('message');
     expect(response.data.message).to.be.equals('Sale not found');
+  });
+
+  it('Testa a função createNewSale de service de sales', async function () {
+    sinon.stub(salesModel, 'createNewSale').resolves({
+      id: 1,
+      itemsSold,
+    });
+
+    const response = await salesService.createNewSale(itemsSold);
+
+    expect(response).to.be.an('object');
+    expect(response).to.have.property('status');
+    expect(response).to.have.property('data');
+    expect(response.status).to.be.equals(201);
+    expect(response.data).to.be.an('object');
+    expect(response.data).to.have.property('id');
+    expect(response.data.id).to.be.equals(1);
+    expect(response.data).to.have.property('itemsSold');
+    expect(response.data.itemsSold).to.be.an('array');
+    expect(response.data.itemsSold).to.have.lengthOf(2);
+    expect(response.data.itemsSold[0]).to.be.an('object');
+    expect(response.data.itemsSold[0]).to.have.property('productId');
+    expect(response.data.itemsSold[0].productId).to.be.equals(1);
+    expect(response.data.itemsSold[0]).to.have.property('quantity');
+    expect(response.data.itemsSold[0].quantity).to.be.equals(5);
   });
 });
