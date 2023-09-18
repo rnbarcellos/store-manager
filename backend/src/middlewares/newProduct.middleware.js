5d84ace7c578d4a3a9980b00d4f17aca009a9228
rnbarcellos <rodrigo.barcellos@outlook.com.br>
2023-09-18 11:20:48 -0300

    :recycle: refactor: add status code
    also remove unnecessary async

--- backend/src/middlewares/newProduct.middleware.js
+++ backend/src/middlewares/newProduct.middleware.js
@@ -1,12 +1,13 @@
 const { validateProduct } = require('../validations/products.validations');
+const { UNPROCESSABLE_ENTITY, BAD_REQUEST } = require('../utils/httpStatusCode');
 
 const handleValidation = (validation, res) => {
-  let status = 400;
-  if (validation.includes('length')) status = 422;
-  return res.status(status).json({ message: validation });
+  let statusCode = UNPROCESSABLE_ENTITY;
+  if (validation.includes('length')) statusCode = BAD_REQUEST;
+  return res.status(statusCode).json({ message: validation });
 };
 
-const validateNewProduct = async (req, res, next) => {
+const validateNewProduct = (req, res, next) => {
   const validation = validateProduct(req.body);
   if (validation) return handleValidation(validation, res);
   next();
